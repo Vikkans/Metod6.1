753e9d0ec5ee3e7cd4a36eab5213527eebdf6c3a
Vikkans <143171920+Vikkans@users.noreply.github.com>
2024-02-14 10:36:54 +0100

    Add project files.

---
+++ ConsoleApp61/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Forre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tal1 = 1;
+            int tal2 = 2;
+            int tal3 = 3;
+            Console.WriteLine(Addera(tal1, tal2, tal3));
+        }
+
+        static int Addera(int tal1, int tal2, int tal3)
+        {
+         
+            return tal1 + tal2 + tal3;
+
+            
+        }
+    }
+}
